bafe9f30805d03bbafe7db24af0cdec96d83f6a7
ggornes <gerardogornes@gmail.com>
2020-06-10 11:40:10 +0800

    Ennabled migrations

---
+++ CookBookData/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CookBookData.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CookBookData.Model.DbContext.CookBookContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CookBookData.Model.DbContext.CookBookContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
