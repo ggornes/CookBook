8c88efd4fdff6e6733f54545bcda6480de8cc1a4
ggornes <gerardogornes@gmail.com>
2020-06-10 13:34:20 +0800

    added RelayCommand class

---
+++ CookBook/ViewModel/Interfaces/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CookBook.ViewModel.Interfaces
+{
+    public class RelayCommand : ICommand
+    {
+        private Predicate<object> _canExecute;
+        private Action<object> _method;
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> method)
+            : this(method, null)
+        {
+        }
+
+        public RelayCommand(Action<object> method, Predicate<object> canExecute)
+        {
+            _method = method;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _method.Invoke(parameter);
+        }
+    }
+}
